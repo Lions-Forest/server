c4199750e53bc0f908dd8756530201639c60e3ca
chaeyeonlee898 <chaeyeonlee898@gmail.com>
2025-11-11 18:41:25 +0900

    fix: firebase 사용하도록 로그인 api 및 설정 수정

--- src/main/java/com/example/lionsforest/domain/user/service/AuthService.java
+++ src/main/java/com/example/lionsforest/domain/user/service/AuthService.java
@@ -6,14 +6,17 @@ import com.example.lionsforest.domain.user.dto.LoginResponseDTO;
 import com.example.lionsforest.domain.user.dto.TokenResponseDTO;
 import com.example.lionsforest.domain.user.dto.UserInfoDTO;
 import com.example.lionsforest.domain.user.repository.UserRepository;
+import com.example.lionsforest.global.component.FirebaseTokenVerifier;
 import com.example.lionsforest.global.component.GoogleTokenVerifier;
 import com.example.lionsforest.global.component.MemberWhitelistValidator;
 import com.example.lionsforest.global.jwt.JwtTokenProvider;
+import com.google.firebase.auth.FirebaseAuth;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.naming.AuthenticationException;
 import java.util.Optional;
 
 @Slf4j
@@ -25,24 +28,35 @@ public class AuthService {
     private final UserRepository userRepository;
     private final MemberWhitelistValidator whitelistValidator;
     private final JwtTokenProvider jwtTokenProvider;
-    private final GoogleTokenVerifier googleTokenVerifier;
+    private final FirebaseTokenVerifier firebaseTokenVerifier;
 
     public LoginResponseDTO googleLoginOrRegister(LoginRequestDTO request) {
 
-        UserInfoDTO googleUserInfo = googleTokenVerifier.verify(request.getIdToken());
-        String name = googleUserInfo.getName();
-        String email = googleUserInfo.getEmail();
+        //request DTO에서 idToken 꺼내기
+        String idToken = request.getIdToken();
+        //firebasetokenverifier가 토큰 검증 -> 사용자 정보 추출
+        UserInfoDTO userInfo;
+        try{
+            userInfo = firebaseTokenVerifier.verifyIdToken(idToken);
+        }catch(AuthenticationException e){
+            log.error("Invalid ID Token: {}", e.getMessage());
+            throw new SecurityException("유효하지 않은 토큰입니다.", e);
+        }
+        String name = userInfo.getName();
+        String email = userInfo.getEmail();
 
+        //동아리 부원인지 조회
         if (!whitelistValidator.isMember(name, email)) {
             throw new SecurityException("동아리 부원 명단에 존재하지 않거나 정보가 일치하지 않습니다.");
         }
 
+        //첫 가입인지 확인한 후 로그인 시킴
         Optional<User> optionalUser = userRepository.findByEmail(email);
         boolean isNewUser = false;
         User user;
 
         if (optionalUser.isEmpty()) {
-            user = googleUserInfo.toEntity();
+            user = userInfo.toEntity();
             userRepository.save(user);
             isNewUser = true;
             System.out.println("새 유저 생성!");
@@ -51,10 +65,10 @@ public class AuthService {
             System.out.println("이미 존재하는 유저");
         }
 
-        // 6. JWT 토큰 생성 (반환 타입이 TokenResponse DTO임)
+        // JWT 토큰 생성 (반환 타입이 TokenResponse DTO임)
         TokenResponseDTO tokens = jwtTokenProvider.createTokens(user.getId(), user.getEmail());
 
-        // 7. 응답 DTO 생성
+        // 응답 DTO 생성
         return LoginResponseDTO.builder()
                 .id(user.getId())
                 .accessToken(tokens.getAccessToken()) // TokenResponse DTO의 getter
