0eccf19ebeb785a41d442d2b2b1946bf72acf001
chaeyeonlee898 <chaeyeonlee898@gmail.com>
2025-11-12 13:29:59 +0900

    fix: 프론트 테스트용 임시로그인 api

--- src/main/java/com/example/lionsforest/global/config/SwaggerConfig.java
+++ src/main/java/com/example/lionsforest/global/config/SwaggerConfig.java
@@ -1,7 +1,6 @@
 package com.example.lionsforest.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -9,14 +8,12 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import io.swagger.v3.oas.models.servers.Server;
 
 import static java.awt.SystemColor.info;
 
-@OpenAPIDefinition(
-        servers = {
-                @Server(url = "https://api.lions-forest.p-e.kr", description = "Production server URL")
-        }
-)
+@OpenAPIDefinition
 @Configuration
 public class SwaggerConfig {
 
@@ -48,5 +45,22 @@ public class SwaggerConfig {
                 .info(info) // 1번 Info 적용
                 .addSecurityItem(securityRequirement) // 3번 SecurityRequirement 적용
                 .components(components);              // 4번 Components 적용
+    }
+
+    // 3. [추가] "development" 프로필일 때 "로컬 서버" 정보를 추가합니다.
+    // (application.yml의 active: development와 일치)
+    @Bean
+    @Profile("development")
+    public OpenAPI developmentServer() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("http://localhost:8080").description("Local Development Server"));
+    }
+
+    // 4. [추가] "deployment" 프로필일 때 "배포 서버" 정보를 추가합니다.
+    @Bean
+    @Profile("deployment")
+    public OpenAPI deploymentServer() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("https://api.lions-forest.p-e.kr").description("Production Server"));
     }
 }
