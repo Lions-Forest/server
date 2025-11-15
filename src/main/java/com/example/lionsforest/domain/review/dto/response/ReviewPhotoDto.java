c2fec6307e5eda84d487495246897cd8308654a4
chaeyeonlee898 <chaeyeonlee898@gmail.com>
2025-11-15 23:00:53 +0900

    fix: 후기 조회 api dto 수정

--- src/main/java/com/example/lionsforest/domain/review/dto/response/ReviewPhotoDto.java
+++ src/main/java/com/example/lionsforest/domain/review/dto/response/ReviewPhotoDto.java
@@ -5,10 +5,12 @@ import lombok.Getter;
 
 @Getter
 public class ReviewPhotoDto {
+    private final Long id;
     private final String photoUrl;
     private final Integer order;
 
     public ReviewPhotoDto(ReviewPhoto photo) {
+        this.id = photo.getId();
         this.photoUrl = photo.getPhoto();
         this.order = photo.getPhoto_order();
     }
